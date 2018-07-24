4b55f9f4d87ef8c629bf401884f0c7befa6b76db
Bernat Bonda <bernat_bonda@epam.com>
2018-07-24 10:42:15 +0200

    GameDataService exploded

--- src/main/java/skyxplore/service/GameDataFacade.java
+++ src/main/java/skyxplore/service/GameDataFacade.java
@@ -1,23 +1,21 @@
 package skyxplore.service;
 
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
-import skyxplore.dataaccess.gamedata.entity.Ship;
+import skyxplore.controller.request.EquipmentCategoryRequest;
 import skyxplore.dataaccess.gamedata.entity.abstractentity.FactoryData;
 import skyxplore.dataaccess.gamedata.entity.abstractentity.GeneralDescription;
 import skyxplore.dataaccess.gamedata.entity.abstractentity.ShopData;
-import skyxplore.dataaccess.gamedata.subservice.*;
 import skyxplore.domain.ship.EquippedShip;
 import skyxplore.domain.slot.EquippedSlot;
-import skyxplore.exception.EquipmentNotFoundException;
-import skyxplore.controller.request.EquipmentCategoryRequest;
-
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
+import skyxplore.service.gamedata.CategoryQueryService;
+import skyxplore.service.gamedata.DataQueryService;
+import skyxplore.service.gamedata.EquipmentDataCollectorService;
 
 @SuppressWarnings("WeakerAccess")
 @Service
@@ -25,161 +23,28 @@ import java.util.stream.Collectors;
 @Slf4j
 //TODO explode
 public class GameDataFacade {
-    private final AbilityService abilityService;
-    private final ArmorService armorService;
-    private final BatteryService batteryService;
-    private final CoreHullService coreHullService;
-    private final ExtenderService extenderService;
-    private final GeneratorService generatorService;
-    private final MaterialService materialService;
-    private final ShieldService shieldService;
-    private final ShipService shipService;
-    private final StorageService storageService;
-    private final WeaponService weaponService;
+    private final CategoryQueryService categoryQueryService;
+    private final DataQueryService dataQueryService;
+    private final EquipmentDataCollectorService equipmentDataCollectorService;
 
-    public Map<String, GeneralDescription> collectEquipmentData(List<String> ids){
-        return ids.stream().map(this::getData).collect(Collectors.toMap(GeneralDescription::getId, g -> g, (k1, k2) -> k1));
+    public Map<String, GeneralDescription> collectEquipmentData(List<String> ids) {
+        return equipmentDataCollectorService.collectEquipmentData(ids);
     }
 
     public Map<String, GeneralDescription> collectEquipmentData(EquippedShip ship, EquippedSlot defenseSlot, EquippedSlot weaponSlot) {
-        Map<String, GeneralDescription> result = new HashMap<>();
-        result.put(ship.getShipType(), getShip(ship.getShipType()));
-        result.putAll(getData(ship.getConnectorEquipped()));
-        result.putAll(getData(defenseSlot));
-        result.putAll(getData(weaponSlot));
-        result.putAll(getData(getShip(ship.getShipType()).getAbility()));
-        return result;
+        return equipmentDataCollectorService.collectEquipmentData(ship, defenseSlot, weaponSlot);
     }
 
-    private Map<String, GeneralDescription> getData(EquippedSlot slot) {
-        Map<String, GeneralDescription> result = new HashMap<>();
-        result.putAll(getData(slot.getFrontEquipped()));
-        result.putAll(getData(slot.getLeftEquipped()));
-        result.putAll(getData(slot.getRightEquipped()));
-        result.putAll(getData(slot.getBackEquipped()));
-        return result;
+    public FactoryData getFactoryData(String elementId) {
+        return dataQueryService.getFactoryData(elementId);
     }
 
-    private Map<String, GeneralDescription> getData(Collection<String> ids) {
-        Map<String, GeneralDescription> result = new HashMap<>();
-        ids.forEach(e -> result.put(e, getData(e)));
-        return result;
+    public Map<String, GeneralDescription> getElementsOfCategory(EquipmentCategoryRequest request) {
+        return categoryQueryService.getElementsOfCategory(request);
     }
 
-    public GeneralDescription getData(String id) {
-        GeneralDescription result = abilityService.get(id);
-        if (result == null) {
-            result = armorService.get(id);
-        }
-        if (result == null) {
-            result = batteryService.get(id);
-        }
-        if (result == null) {
-            result = coreHullService.get(id);
-        }
-        if (result == null) {
-            result = extenderService.get(id);
-        }
-        if (result == null) {
-            result = generatorService.get(id);
-        }
-        if (result == null) {
-            result = materialService.get(id);
-        }
-        if (result == null) {
-            result = shieldService.get(id);
-        }
-        if (result == null) {
-            result = shipService.get(id);
-        }
-        if (result == null) {
-            result = storageService.get(id);
-        }
-        if (result == null) {
-            result = weaponService.get(id);
-        }
-        if (result == null) {
-            throw new EquipmentNotFoundException("Equipment not found with id " + id);
-        }
-        return result;
+    public ShopData findBuyable(String elementId) {
+        return dataQueryService.findBuyable(elementId);
     }
 
-    public FactoryData getFactoryData(String elementId){
-        GeneralDescription element = getData(elementId);
-        if(element instanceof FactoryData){
-            return (FactoryData) element;
-        }
-        throw new IllegalArgumentException(elementId + " is not instance of FactoryData");
-    }
-
-    public ShopData findBuyable(String elementId){
-        GeneralDescription data = getData(elementId);
-        if(data instanceof ShopData){
-            return (ShopData) data;
-        }
-        throw new IllegalArgumentException(elementId + " is not instance of ShopData.");
-    }
-
-    public Map<String, GeneralDescription> getElementsOfCategory(EquipmentCategoryRequest category) {
-        log.info("Querying equipments of category {}", category);
-        Map<String, GeneralDescription> result = new HashMap<>();
-        switch (category) {
-            case ALL:
-                result.putAll(batteryService);
-                result.putAll(generatorService);
-                result.putAll(extenderService);
-                result.putAll(coreHullService);
-                result.putAll(storageService);
-                result.putAll(weaponService);
-                result.putAll(armorService);
-                result.putAll(shieldService);
-                result.putAll(shipService);
-                result.putAll(materialService);
-                break;
-            case MATERIAL:
-                    result.putAll(materialService);
-                break;
-            case CONNECTOR:
-                result.putAll(batteryService);
-                result.putAll(generatorService);
-                result.putAll(extenderService);
-                result.putAll(coreHullService);
-                result.putAll(storageService);
-                break;
-            case ENERGY:
-                result.putAll(batteryService);
-                result.putAll(generatorService);
-                break;
-            case EXTENDER:
-                result.putAll(extenderService);
-                break;
-            case COREHULL:
-                result.putAll(coreHullService);
-                break;
-            case STORAGE:
-                result.putAll(storageService);
-                break;
-            case DEFENSE:
-                result.putAll(armorService);
-                result.putAll(shieldService);
-                break;
-            case ARMOR:
-                result.putAll(armorService);
-                break;
-            case SHIELD:
-                result.putAll(shieldService);
-                break;
-            case SHIP:
-                result.putAll(shipService);
-                break;
-            case WEAPON:
-                result.putAll(weaponService);
-                break;
-        }
-        return result;
-    }
-
-    public Ship getShip(String id) {
-        return shipService.get(id);
-    }
 }
