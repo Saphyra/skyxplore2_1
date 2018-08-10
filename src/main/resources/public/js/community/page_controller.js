9e96b9b1bbcc9e6789222954b3e0cd7658eda432
Bernat Bonda <bernat_bonda@epam.com>
2018-08-10 11:05:24 +0200

    Switch labels

--- src/main/resources/public/js/community/page_controller.js
+++ src/main/resources/public/js/community/page_controller.js
@@ -1,5 +1,37 @@
 (function PageController(){
     window.pageController = new function(){
-
+        this.showAddFriendWindow = showAddFriendWindow;
+        this.showBlockedUsers = showBlockedUsers;
+        this.showFriendList = showFriendList;
     }
+
+    function showAddFriendWindow(){
+        try{
+            $(".mainlabel").hide();
+            $("#addfriend").show();
+        }catch(err){
+             const message = arguments.callee.name + " - " + err.name + ": " + err.message;
+             logService.log(message, "error");
+         }
+     }
+
+    function showBlockedUsers(){
+        try{
+            $(".friendlistlabel").hide();
+            $("#blockedusers").show();
+        }catch(err){
+             const message = arguments.callee.name + " - " + err.name + ": " + err.message;
+             logService.log(message, "error");
+         }
+     }
+
+     function showFriendList(){
+         try{
+             $(".friendlistlabel").hide();
+             $("#listfriends").show();
+         }catch(err){
+              const message = arguments.callee.name + " - " + err.name + ": " + err.message;
+              logService.log(message, "error");
+          }
+      }
 })();
