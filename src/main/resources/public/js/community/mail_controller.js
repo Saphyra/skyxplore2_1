899009cc1187a7531ae5d02912fce9b587929a7e
Bernat Bonda <bondabear@gmail.com>
2018-08-31 19:22:34 +0200

    Show number of unread mails on community page

--- src/main/resources/public/js/community/mail_controller.js
+++ src/main/resources/public/js/community/mail_controller.js
@@ -1,5 +1,21 @@
 (function MailController(){
     window.mailController = new function(){
+        scriptLoader.loadScript("js/common/dao/community_dao.js");
         
+        $(document).ready(function(){
+            displayNumberOfUnreadMails();
+        });
+    }
+    
+    function displayNumberOfUnreadMails(){
+        try{
+            const numberOfUnreadMails = communityDao.getNumberOfUnreadMails();
+            if(numberOfUnreadMails > 0){
+                document.getElementById("numberofunreadmails").innerHTML = " (" + numberOfUnreadMails + ")";
+            }
+        }catch(err){
+            const message = arguments.callee.name + " - " + err.name + ": " + err.message;
+            logService.log(message, "error");
+        }
     }
 })();
