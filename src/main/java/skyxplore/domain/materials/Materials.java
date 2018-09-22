c95cf42153ec4681af1d3b5c2c31dd050d47d789
Bernat Bonda <bondabear@gmail.com>
2018-09-22 21:56:58 +0200

    MaterialsTest

--- src/main/java/skyxplore/domain/materials/Materials.java
+++ src/main/java/skyxplore/domain/materials/Materials.java
@@ -11,56 +11,54 @@ import java.util.Set;
 @SuppressWarnings("WeakerAccess")
 @NoArgsConstructor
 @Slf4j
-//TODO unit test
 public class Materials extends HashMap<String, Integer> {
 
-    public Materials(Map<String, Integer> elements){
+    public Materials(Map<String, Integer> elements) {
         putAll(elements);
     }
 
     @Override
-    public Integer get(Object key){
-        if(!containsKey(key)){
+    public Integer get(Object key) {
+        if (!containsKey(key)) {
             return 0;
         }
         return super.get(key);
     }
 
-    public Integer addMaterial(String key, Integer value){
+    public Integer addMaterial(String key, Integer value) {
         return super.put(key, get(key) + value);
     }
 
-    public Integer removeMaterial(String key, Integer amount){
+    public Integer removeMaterial(String key, Integer amount) {
         Integer actual = get(key);
-        log.info("{} amount: {}", key, amount);
-        log.info("{} actual: {}", key, actual);
-        if(actual < amount){
-            throw  new NotEnoughMaterialsException("Not enough " + key +". Needed: " + amount + ", have: " + actual);
+        log.info("removeMaterial - {} toRemove: {}, actual: {}", key, amount, actual);
+        if (actual < amount) {
+            throw new NotEnoughMaterialsException("Not enough " + key + ". Needed: " + amount + ", have: " + actual);
         }
         actual -= amount;
-        put(key, actual);
-        log.info("{} after change: {}", key, get(key));
-        return  actual;
+        super.put(key, actual);
+        log.debug("{} after change: {}", key, get(key));
+        return get(key);
     }
 
     @Override
-    public Integer put(String key, Integer value){
+    public Integer put(String key, Integer value) {
         return addMaterial(key, value);
     }
 
     @Override
-    public void putAll(Map<? extends String, ? extends Integer> map){
-        Set<?extends String> keySet = map.keySet();
+    public void putAll(Map<? extends String, ? extends Integer> map) {
+        Set<? extends String> keySet = map.keySet();
         keySet.forEach(k -> put(k, map.get(k)));
     }
 
     @Override
-    public Integer remove(Object key){
+    public Integer remove(Object key) {
         throw new UnsupportedOperationException("You cannot remove elements.");
     }
 
     @Override
-    public void clear(){
+    public void clear() {
         throw new UnsupportedOperationException("You cannot remove elements.");
     }
 }
