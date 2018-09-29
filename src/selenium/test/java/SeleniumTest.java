3d1683f07648ff8d4f2587ff813a5c0bf0a1a32d
Bernat Bonda <bondabear@gmail.com>
2018-09-29 22:26:53 +0200

    Repackage

--- src/selenium/test/java/SeleniumTest.java
+++ src/selenium/test/java/SeleniumTest.java
@@ -1,11 +1,14 @@
-package selenium;
+package test.java;
 
+import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SeleniumTest {
+
+    @Test
     public void test() throws InterruptedException {
-        System.setProperty("webdriver.chrome.driver", getClass().getClassLoader().getResource("chromedriver.exe").getPath());
+        System.setProperty("webdriver.chrome.driver", getClass().getClassLoader().getResource("resources/chromedriver.exe").getPath());
         WebDriver driver = new ChromeDriver();
         driver.get("localhost:8080");
         Thread.sleep(10000);
