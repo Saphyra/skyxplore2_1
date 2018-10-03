f93b43fca65c769c22c6451e32289634aada0342
Bernat Bonda <bernat_bonda@epam.com>
2018-10-03 14:06:29 +0200

    Converters deal with optionals

--- src/main/java/skyxplore/domain/Converter.java
+++ src/main/java/skyxplore/domain/Converter.java
@@ -1,12 +1,16 @@
 package skyxplore.domain;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface Converter<E, D> {
     D convertEntity(E entity);
+    Optional<D> convertEntity(Optional<E> entity);
+    Optional<D> convertEntityToOptional(E entity);
     List<D> convertEntity(List<E> entity);
 
     E convertDomain(D domain);
+    Optional<E> convertDomain(Optional<D> domain);
+    Optional<E> convertDomainToOptional(D domain);
     List<E> convertDomain(List<D> domain);
-
 }
--- src/main/java/skyxplore/domain/ConverterBase.java
+++ src/main/java/skyxplore/domain/ConverterBase.java
@@ -1,21 +1,41 @@
 package skyxplore.domain;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public abstract class ConverterBase<E, D> implements Converter<E, D> {
+    @Override
+    public Optional<D> convertEntity(Optional<E> entity) {
+        return convertEntityToOptional(entity.orElse(null));
+    }
+
+    @Override
+    public Optional<D> convertEntityToOptional(E entity) {
+        return Optional.ofNullable(convertEntity(entity));
+    }
 
     @Override
     public List<D> convertEntity(List<E> entity) {
-        if(entity == null){
-            return  null;
+        if (entity == null) {
+            return null;
         }
         return entity.stream().map(this::convertEntity).collect(Collectors.toList());
     }
 
     @Override
+    public Optional<E> convertDomain(Optional<D> domain) {
+        return convertDomainToOptional(domain.orElseThrow(() -> new IllegalArgumentException("domain must not be null.")));
+    }
+
+    @Override
+    public Optional<E> convertDomainToOptional(D domain) {
+        return Optional.of(convertDomain(domain));
+    }
+
+    @Override
     public List<E> convertDomain(List<D> domain) {
-        if(domain == null){
+        if (domain == null) {
             throw new IllegalArgumentException("domain must not be null.");
         }
         return domain.stream().map(this::convertDomain).collect(Collectors.toList());
