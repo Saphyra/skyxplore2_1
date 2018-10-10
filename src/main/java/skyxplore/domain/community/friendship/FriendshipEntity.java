df23e7a68d1addc889e8fba2161c268564291e1b
Bernat Bonda <bondabear@gmail.com>
2018-10-10 23:03:17 +0200

    Entity length

--- src/main/java/skyxplore/domain/community/friendship/FriendshipEntity.java
+++ src/main/java/skyxplore/domain/community/friendship/FriendshipEntity.java
@@ -1,22 +1,23 @@
 package skyxplore.domain.community.friendship;
 
-import javax.persistence.Entity;
-
-import javax.persistence.*;
-
 import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "friendship")
 @Data
 public class FriendshipEntity {
     @Id
-    @Column(name = "friendship_id")
+    @Column(name = "friendship_id", length = 50)
     private String friendshipId;
 
-    @Column(name = "character_id")
+    @Column(name = "character_id", length = 50)
     private String characterId;
 
-    @Column(name = "friend_id")
+    @Column(name = "friend_id", length = 50)
     private String friendId;
 }
