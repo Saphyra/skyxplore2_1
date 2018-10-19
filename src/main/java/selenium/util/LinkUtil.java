6bc5d527b1dec660cd55ae960fd4c05a6191873c
Bernat Bonda <bernat_bonda@epam.com>
2018-10-19 08:36:06 +0200

    Host switch

--- src/main/java/selenium/util/LinkUtil.java
+++ src/main/java/selenium/util/LinkUtil.java
@@ -1,8 +1,8 @@
 package selenium.util;
 
 public class LinkUtil {
-    public static final String HOST_2 = "http://localhost:8080/";
-    public static final String HOST = "http://92.249.138.240:8080/";
+    public static final String HOST = "http://localhost:8080/";
+    public static final String HOST_2 = "http://92.249.138.240:8080/";
     public static final String ACCOUNT = HOST + "account";
     public static final String CHARACTER_SELECT = HOST + "characterselect";
 }
