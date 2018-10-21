dbd10b1faf9fe7926f94037eb946557a0dd02022
Bernat Bonda <bondabear@gmail.com>
2018-10-21 18:55:42 +0200

    Test character name

--- src/main/java/selenium/domain/SeleniumCharacter.java
+++ src/main/java/selenium/domain/SeleniumCharacter.java
@@ -1,18 +1,18 @@
 package selenium.domain;
 
-import static selenium.util.StringUtil.crop;
-import static selenium.util.UserUtil.randomUID;
-import static skyxplore.controller.request.character.CreateCharacterRequest.CHARACTER_NAME_MAX_LENGTH;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import static selenium.util.StringUtil.crop;
+import static selenium.util.UserUtil.randomUID;
+import static skyxplore.controller.request.character.CreateCharacterRequest.CHARACTER_NAME_MAX_LENGTH;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class SeleniumCharacter {
-    private static final String CHARACTER_NAME_PREFIX = "character";
+    private static final String CHARACTER_NAME_PREFIX = "character-";
 
     private String characterName;
 
