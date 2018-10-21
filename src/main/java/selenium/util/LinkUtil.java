8cca6001e64f703287fd1ff383d55d35c3093932
Bernat Bonda <bondabear@gmail.com>
2018-10-21 18:56:02 +0200

    Host switch

--- src/main/java/selenium/util/LinkUtil.java
+++ src/main/java/selenium/util/LinkUtil.java
@@ -3,7 +3,7 @@ package selenium.util;
 public class LinkUtil {
     public static final String HOST_LOCAL = "http://localhost:8080/";
     public static final String HOST_PI_INTERNAL = "http://192.168.0.200:9001/";
-    public static final String HOST = HOST_PI_INTERNAL;
+    public static final String HOST = HOST_LOCAL;
 
     public static final String ACCOUNT = HOST + "account";
     public static final String CHARACTER_SELECT = HOST + "characterselect";
