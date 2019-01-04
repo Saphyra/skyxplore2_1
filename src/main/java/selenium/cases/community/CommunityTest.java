d7416ecf0b5bcd6c267d5ab3f8903cf92c6a8242
Bernat Bonda <bondabear@gmail.com>
2019-01-04 12:33:28 +0100

    Comments / todos

--- src/main/java/selenium/cases/community/CommunityTest.java
+++ src/main/java/selenium/cases/community/CommunityTest.java
@@ -28,6 +28,48 @@ public class CommunityTest {
     public static void run(WebDriver driver) {
         CommunityTest testCase = new CommunityTest(driver);
         testCase.init();
+
+        /*
+        Add friend test
+            - Selectable characters
+                - Own character(s) do not appear
+                - Search by name
+                    - Only matching ones appear
+            - Send friend request
+                - Character 1: Request appears at sent
+                - Character 2: Request appears at new ones
+                    - - Notifications shown
+                - Characters do not appear at selectable characters (from both side)
+            - Cancel friend request
+                - Request disappears both side
+                - New friendRequest can be sent.
+            - Accept friend request
+                - Friendship appears
+                - FriendRequest disappears
+        Send Mail test
+            - Selectable characters
+                - Search by name
+                    - Only matching ones appear
+            - Send mail
+                - Mail appears at sent ones
+                - Character 2 gets a new mail
+                    - Notifications shown
+                - Character 2 reads the mail
+                    - Notifications disappear
+            - Archive mail
+                - Mail moved to archived
+            - Delete mail
+                - Mail disappears
+                - Other character still see the mail
+        Block character test
+            - block character
+                - Existing friendship disappears
+                - Cannot send friendRequest
+                - Cannot send mail
+            - unblock character
+                - Friend request can be sent
+                - Mail can be sent
+         */
     }
 
     private void init() {
