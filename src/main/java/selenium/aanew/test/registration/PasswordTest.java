0993f9ee7f4f1e8a56ab80f1b2f8f81581132fc8
Bernat Bonda <bondabear@gmail.com>
2019-01-10 22:11:11 +0100

    TooShortPasswordTest

---
+++ src/main/java/selenium/aanew/test/registration/PasswordTest.java
@@ -0,0 +1,33 @@
+package selenium.aanew.test.registration;
+
+import org.junit.Test;
+import selenium.aanew.SeleniumTestApplication;
+import selenium.aanew.logic.page.IndexPage;
+import selenium.aanew.logic.validator.FieldValidator;
+import selenium.aanew.test.registration.password.TooShortPasswordTest;
+import selenium.aanew.test.registration.password.helper.PasswordTestHelper;
+
+import static selenium.aanew.logic.util.LinkUtil.HOST;
+
+public class PasswordTest extends SeleniumTestApplication {
+    private PasswordTestHelper passwordTestHelper;
+    private IndexPage indexPage;
+    private FieldValidator fieldValidator;
+
+    @Override
+    protected void init() {
+        indexPage = new IndexPage(driver);
+        passwordTestHelper = new PasswordTestHelper(driver, indexPage);
+        fieldValidator = new FieldValidator(driver, HOST);
+    }
+
+    @Test
+    public void testTooShortPassword(){
+        TooShortPasswordTest.builder()
+            .passwordTestHelper(passwordTestHelper)
+            .indexPage(indexPage)
+            .fieldValidator(fieldValidator)
+            .build()
+            .testTooShortPassword();
+    }
+}
---
+++ src/main/java/selenium/aanew/test/registration/password/helper/PasswordTestHelper.java
@@ -0,0 +1,27 @@
+package selenium.aanew.test.registration.password.helper;
+
+import lombok.RequiredArgsConstructor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import selenium.aanew.logic.domain.SeleniumUser;
+import selenium.aanew.logic.page.IndexPage;
+
+@RequiredArgsConstructor
+public class PasswordTestHelper {
+    private final WebDriver driver;
+    private final IndexPage indexPage;
+
+    public void setUpForPasswordTest(SeleniumUser user) {
+        WebElement userNameField = indexPage.getRegistrationUserNameField();
+        userNameField.sendKeys(user.getUserName());
+
+        WebElement emailElement = indexPage.getRegistrationEmailField();
+        emailElement.sendKeys(user.getEmail());
+
+        WebDriverWait webDriverWait = new WebDriverWait(driver, 10);
+        webDriverWait.until(ExpectedConditions.invisibilityOf(indexPage.getInvalidUserNameField()));
+        webDriverWait.until(ExpectedConditions.invisibilityOf(indexPage.getInvalidEmailField()));
+    }
+}
