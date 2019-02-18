901d2289b574a76107b2d48891e6922569e4504c
Bernat Bonda <bondabear@gmail.com>
2019-02-18 22:06:27 +0100

    Order storage alphabetically

--- src/main/resources/public/js/equipment/equip_service.js
+++ src/main/resources/public/js/equipment/equip_service.js
@@ -2,14 +2,26 @@
     events.UNEQUIP_ITEM = "unequip_item";
     events.ITEM_UNEQUIPPED = "item_unequipped";
     
+    events.EQUIP_SHIP = "equip_ship";
+    events.SHIP_EQUIPPED = "ship_equipped";
+    
     eventProcessor.registerProcessor(new EventProcessor(
         function(eventType){return eventType == events.UNEQUIP_ITEM},
         unequip
     ));
     
+    eventProcessor.registerProcessor(new EventProcessor(
+        function(eventType){return eventType == events.EQUIP_SHIP},
+        equipShip
+    ));
+    
     function unequip(event){
         const payload = event.getPayload();
-        
         eventProcessor.processEvent(new Event(events.ITEM_UNEQUIPPED, payload));
+    }
+    
+    function equipShip(event){
+        const payload = event.getPayload();
+        eventProcessor.processEvent(new Event(events.SHIP_EQUIPPED, payload));
     }
 })();
--- src/main/resources/public/js/equipment/storage_service.js
+++ src/main/resources/public/js/equipment/storage_service.js
@@ -5,8 +5,9 @@
     scriptLoader.loadScript("js/common/equipment/equipment_label_service.js");
     
     events.LOAD_STORAGE = "load_storage";
+    events.ADD_TO_STORAGE = "add_to_storage";
     
-    const categories = {};
+    const categories = [];
     
     eventProcessor.registerProcessor(new EventProcessor(
         function(eventType){return eventType === events.LOAD_STORAGE},
@@ -14,7 +15,10 @@
     ));
     
     eventProcessor.registerProcessor(new EventProcessor(
-        function(eventType){return eventType == events.ITEM_UNEQUIPPED},
+        function(eventType){
+            return eventType == events.ITEM_UNEQUIPPED
+                || eventType == events.ADD_TO_STORAGE;
+        },
         function(event){
             displayItem(event.getPayload().itemId);
         }
@@ -37,11 +41,24 @@
         $("#empty-storage").hide();
         
         const itemData = itemCache.get(itemId);
-        if(!categories[itemData.category]){
-            createCategory(itemData.category);
+        let category = getCategory(itemData.category);
+        if(!category){
+            category = createCategory(itemData.category);
         }
         
-        categories[itemData.category].addItem(itemData);
+        category.addItem(itemData);
+        
+        function getCategory(categoryId){
+            let result = null;
+            for(let cindex in categories){
+                const category = categories[cindex];
+                if(category.getId() == categoryId){
+                    result = category;
+                    break;
+                }
+            }
+            return result;
+        }
         
         function createCategory(categoryId){
             const container = document.createElement("DIV");
@@ -58,20 +75,61 @@
             
             document.getElementById("equipment-list").appendChild(container);
             
-            categories[categoryId] = new Category(container, equipmentListContainer);
+            const nextCategoryIndex = findNextCategoryAlphabetically(categoryId);
+            const nextCategory = categories[nextCategoryIndex] || null;
+            document.getElementById("equipment-list").insertBefore(container, nextCategory ? nextCategory.getContainer() : null);
+            
+            const category = new Category(categoryId, container, equipmentListContainer)
+            categories.splice(nextCategoryIndex, 0, category);
+            
+            return category;
+            
+            function findNextCategoryAlphabetically(categoryId){
+                const categoryName = CategoryNames.getCategoryName(categoryId);
+                let result = 0;
+                for(result; result < categories.length; result++){
+                    const nextCategoryName = CategoryNames.getCategoryName(categories[result].getId());
+                    if(categoryName.localeCompare(nextCategoryName) < 0){
+                        break;
+                    }
+                }
+                return result;
+            }
         }
     }
     
-    function Category(container, list){
+    function Category(id, container, list){
+        const categoryId = id;
         const categoryContainer = container;
         const listContainer = list;
-        const items = {};
+        const items = [];
+        
+        this.getContainer = function(){
+            return categoryContainer;
+        }
+        
+        this.getId = function(){
+            return categoryId;
+        }
         
         this.addItem = function(itemData){
-            if(!items[itemData.id]){
-                createItem(itemData);
+            let item = getItem(itemData.id);
+            if(!item){
+                item = createItem(itemData);
             }
-            items[itemData.id].increaseAmount();
+            item.increaseAmount();
+        }
+        
+        function getItem(itemId){
+            let result = null;
+            for(iIndex in items){
+                const item = items[iIndex];
+                if(item.getId() == itemId){
+                    result = item;
+                    break;
+                }
+            }
+            return result;
         }
         
         function createItem(itemData){
@@ -94,15 +152,43 @@
                 titleContainer.appendChild(amountSuffix);
             container.appendChild(titleContainer);
             
-            items[itemData.id] = new Item(listContainer, container, amountElement);
+            const nextItemIndex = findNextItemAlphabetically(itemData.id);
+            const nextItem = items[nextItemIndex] || null;
+            listContainer.insertBefore(container, nextItem ? nextItem.getContainer() : null);
+            
+            const item = new Item(itemData.id, listContainer, container, amountElement);
+            items.splice(nextItemIndex, 0, item);
+            
+            return item;
+            
+            function findNextItemAlphabetically(itemId){
+                const itemName = Items.getItem(itemId).name;
+                let result = 0;
+                for(result; result < items.length; result++){
+                    const nextItemName = Items.getItem(items[result].getId()).name;
+                    if(itemName.localeCompare(nextItemName) < 0){
+                        break;
+                    }
+                }
+                return result;
+            }
         }
     }
     
-    function Item(parent, container, amount){
+    function Item(id, parent, container, amount){
+        const itemId = id;
         const categoryList = parent;
         const itemContainer = container;
         const amountElement = amount;
         let currentAmount = 0;
+        
+        this.getId = function(){
+            return itemId;
+        }
+        
+        this.getContainer = function(){
+            return itemContainer;
+        }
         
         parent.appendChild(container);
         
