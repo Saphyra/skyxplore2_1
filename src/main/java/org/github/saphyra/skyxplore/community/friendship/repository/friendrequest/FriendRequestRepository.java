57f241c30f6634b245ce06a854a0cd97836ebbb1
Bernat Bonda <bondabear@gmail.com>
2019-05-04 13:38:00 +0200

    FriendRequestRepositoryTest

--- src/main/java/org/github/saphyra/skyxplore/community/friendship/repository/friendrequest/FriendRequestRepository.java
+++ src/main/java/org/github/saphyra/skyxplore/community/friendship/repository/friendrequest/FriendRequestRepository.java
@@ -1,17 +1,15 @@
 package org.github.saphyra.skyxplore.community.friendship.repository.friendrequest;
 
-import java.util.List;
-
-import javax.transaction.Transactional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
+import java.util.List;
+
 @Repository
-//TODO unit test
 interface FriendRequestRepository extends JpaRepository<FriendRequestEntity, String> {
     @Modifying
     @Transactional
