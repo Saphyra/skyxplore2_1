e0f5bbc1b80a4a21ab2078543a3f540ced2f4cfb
Bernat Bonda <bondabear@gmail.com>
2019-05-05 10:28:37 +0200

    CredentialsRepositoryTest

--- src/main/java/org/github/saphyra/skyxplore/user/repository/credentials/CredentialsEntity.java
+++ src/main/java/org/github/saphyra/skyxplore/user/repository/credentials/CredentialsEntity.java
@@ -1,13 +1,13 @@
 package org.github.saphyra.skyxplore.user.repository.credentials;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
@@ -19,9 +19,9 @@ class CredentialsEntity {
     @Column(name = "user_id", length = 50)
     private String userId;
 
-    @Column(name = "user_name")
+    @Column(name = "user_name", nullable = false)
     private String userName;
 
-    @Column(name = "password")
+    @Column(name = "password", nullable = false)
     private String password;
 }
--- src/main/java/org/github/saphyra/skyxplore/user/repository/credentials/CredentialsRepository.java
+++ src/main/java/org/github/saphyra/skyxplore/user/repository/credentials/CredentialsRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-//TODO unit test
 interface CredentialsRepository extends JpaRepository<CredentialsEntity, String> {
     CredentialsEntity getByUserName(String userName);
 }
