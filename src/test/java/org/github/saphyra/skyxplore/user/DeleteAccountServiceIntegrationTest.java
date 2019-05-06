3ec69acf15381acce49f12857ab1a7934565373d
Bernat Bonda <bernat_bonda@epam.com>
2019-05-06 08:21:37 +0200

    Add AccessToken to deleteTest

--- src/test/java/org/github/saphyra/skyxplore/user/DeleteAccountServiceIntegrationTest.java
+++ src/test/java/org/github/saphyra/skyxplore/user/DeleteAccountServiceIntegrationTest.java
@@ -1,8 +1,16 @@
 package org.github.saphyra.skyxplore.user;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.github.saphyra.encryption.impl.PasswordService;
-import com.github.saphyra.util.IdGenerator;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+
+import org.github.saphyra.skyxplore.auth.domain.SkyXpAccessToken;
+import org.github.saphyra.skyxplore.auth.repository.AccessTokenDao;
+import org.github.saphyra.skyxplore.common.DateTimeUtil;
 import org.github.saphyra.skyxplore.common.ObjectMapperDelegator;
 import org.github.saphyra.skyxplore.testing.configuration.DataSourceConfiguration;
 import org.github.saphyra.skyxplore.user.domain.AccountDeleteRequest;
@@ -12,10 +20,12 @@ import org.github.saphyra.skyxplore.user.repository.credentials.CredentialsDao;
 import org.github.saphyra.skyxplore.user.repository.user.UserDao;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
@@ -24,9 +34,9 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.BDDMockito.given;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.saphyra.encryption.impl.PasswordService;
+import com.github.saphyra.util.IdGenerator;
 
 
 @RunWith(SpringRunner.class)
@@ -35,9 +45,9 @@ import static org.mockito.BDDMockito.given;
 public class DeleteAccountServiceIntegrationTest {
     private static final String PASSWORD = "password";
     private static final String USER_ID = "user_id";
+    private static final String ACCESS_TOKEN_ID = "access_token_id";
+    private static final OffsetDateTime NOW = OffsetDateTime.now(ZoneOffset.UTC);
 
-    @MockBean
-    private PasswordService passwordService;
 
     @Autowired
     private DeleteAccountService deleteAccountService;
@@ -48,22 +58,21 @@ public class DeleteAccountServiceIntegrationTest {
     @Autowired
     private CredentialsDao credentialsDao;
 
+    @Autowired
+    private AccessTokenDao accessTokenDao;
+
     @Test
     public void testDeleteAccount() {
         //GIVEN
-        SkyXpUser user = SkyXpUser.builder()
-            .userId(USER_ID)
-            .email("")
-            .build();
-        userDao.save(user);
-
-        SkyXpCredentials credentials = new SkyXpCredentials();
-        credentials.setUserId(USER_ID);
-        credentials.setPassword(PASSWORD);
-        credentials.setUserName("");
-        credentialsDao.save(credentials);
-
-        given(passwordService.authenticate(anyString(), anyString())).willReturn(true);
+        saveCharacter();
+        saveCredentials();
+        saveAccessToken();
+        //TODO blockedcharacter
+        //TODO friendrequest
+        //TODO friendsip
+        //TODO mail
+        //TODO ship
+        //TODO slot
 
         AccountDeleteRequest accountDeleteRequest = new AccountDeleteRequest(PASSWORD);
         //WHEN
@@ -71,14 +80,44 @@ public class DeleteAccountServiceIntegrationTest {
         //THEN
         assertThat(userDao.findById(USER_ID)).isEmpty();
         assertThat(credentialsDao.findById(USER_ID)).isEmpty();
+        assertThat(accessTokenDao.findByUserId(USER_ID)).isEmpty();
+    }
+
+    private void saveAccessToken() {
+        SkyXpAccessToken accessToken = SkyXpAccessToken.builder()
+            .userId(USER_ID)
+            .accessTokenId(ACCESS_TOKEN_ID)
+            .lastAccess(NOW)
+            .build();
+        accessTokenDao.save(accessToken);
+    }
+
+    private void saveCredentials() {
+        SkyXpCredentials credentials = new SkyXpCredentials();
+        credentials.setUserId(USER_ID);
+        credentials.setPassword(PASSWORD);
+        credentials.setUserName("");
+        credentialsDao.save(credentials);
+    }
+
+    private void saveCharacter() {
+        SkyXpUser user = SkyXpUser.builder()
+            .userId(USER_ID)
+            .email("")
+            .build();
+        userDao.save(user);
     }
 
     @TestConfiguration
-    @ComponentScan(basePackageClasses = DeleteAccountService.class)
-    @EnableJpaRepositories(basePackages = "org.github.saphyra.skyxplore.user")
+    @EnableJpaRepositories(basePackages = "org.github.saphyra.skyxplore")
     @EnableTransactionManagement
-    @EntityScan(basePackages = "org.github.saphyra.skyxplore.user")
+    @EntityScan(basePackages = "org.github.saphyra.skyxplore")
     @Import(DataSourceConfiguration.class)
+    @ComponentScan(basePackageClasses = {
+        UserDao.class,
+        CredentialsDao.class,
+        AccessTokenDao.class
+    })
     static class TestConfig {
         @Bean
         public ObjectMapper objectMapper() {
@@ -88,6 +127,34 @@ public class DeleteAccountServiceIntegrationTest {
         @Bean
         public ObjectMapperDelegator objectMapperDelegator(ObjectMapper objectMapper) {
             return new ObjectMapperDelegator(objectMapper);
+        }
+
+        @Bean
+        public DateTimeUtil dateTimeUtil() {
+            return new DateTimeUtil();
+        }
+
+        @Bean
+        public DeleteAccountService deleteAccountService(
+            PasswordService passwordService,
+            CredentialsService credentialsService,
+            ApplicationEventPublisher applicationEventPublisher
+        ) {
+            return new DeleteAccountService(passwordService, credentialsService, applicationEventPublisher);
+        }
+
+        @Bean
+        public CredentialsService credentialsService() {
+            CredentialsService credentialsService = Mockito.mock(CredentialsService.class);
+            given(credentialsService.findByUserId(USER_ID)).willReturn(new SkyXpCredentials(USER_ID, "", PASSWORD));
+            return credentialsService;
+        }
+
+        @Bean
+        public PasswordService passwordService() {
+            PasswordService passwordService = mock(PasswordService.class);
+            given(passwordService.authenticate(anyString(), anyString())).willReturn(true);
+            return passwordService;
         }
 
         @SuppressWarnings("unused")
