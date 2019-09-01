2a8548606745b37d7e85b39705913f42ed69f7c7
Bernat Bonda <bondabear@gmail.com>
2019-09-01 18:10:26 +0200

    Set locale on account page

--- src/main/resources/public/js/account/page_controller.js
+++ src/main/resources/public/js/account/page_controller.js
@@ -3,7 +3,8 @@
     scriptLoader.loadScript("/js/account/change_password/change_password_controller.js");
     scriptLoader.loadScript("/js/account/change_username/change_username_controller.js");
     scriptLoader.loadScript("/js/account/delete_account/delete_account_controller.js");
-        
+    scriptLoader.loadScript("/js/account/set_language/set_language_controller.js");
+
     $(document).ready(function(){
         init();
     });
