38affd70a7c16783c0db0bd4db71d848435572a9
Bernat Bonda <bondabear@gmail.com>
2019-09-15 12:18:23 +0200

    Fix

--- src/main/java/com/github/saphyra/skyxplore/userdata/settings/repository/UserSettingsKeyEntity.java
+++ src/main/java/com/github/saphyra/skyxplore/userdata/settings/repository/UserSettingsKeyEntity.java
@@ -1,14 +1,15 @@
 package com.github.saphyra.skyxplore.userdata.settings.repository;
 
-import java.io.Serializable;
-
-import javax.persistence.Embeddable;
-
 import com.github.saphyra.skyxplore.userdata.settings.domain.UserSettingKey;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import javax.persistence.Embeddable;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import java.io.Serializable;
 
 @Embeddable
 @NoArgsConstructor
@@ -17,5 +18,7 @@ import lombok.NoArgsConstructor;
 @Data
 class UserSettingsKeyEntity implements Serializable {
     private String userId;
+
+    @Enumerated(EnumType.STRING)
     private UserSettingKey settingKey;
 }
